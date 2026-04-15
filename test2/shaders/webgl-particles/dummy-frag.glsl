b15a33e06b298693219d3bc294707df086bc6a2f
Ge You <youge071@gmail.com>
2026-04-15 09:35:13 +0100

    ud

--- test2/shaders/webgl-particles/dummy-frag.glsl
+++
@@ -1,4 +0,0 @@
-#version 300 es
-// WebGL2 requires a fragment shader even when RASTERIZER_DISCARD is active.
-// This no-op satisfies that requirement for the Transform Feedback update pass.
-void main() {}
